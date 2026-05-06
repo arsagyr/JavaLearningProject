e3c7f4d674fb47976f4f7a69f473967ef6ac09e5
DzMaks <rattlemaniac@gmail.com>
2026-05-06 15:55:06 +0300

    Написал класс рандом

---
+++ src/main/java/ru/javastudy/input/RandomInput.java
@@ -0,0 +1,67 @@
+package ru.javastudy.input;
+
+import ru.javastudy.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class RandomInput implements InputStrategy {
+
+    private static final String[] SYLLABLES = {
+            "нан", "тат", "мир", "лор", "сен", "кар", "бел", "дим", "пол", "вер",
+            "тан", "мар", "сол", "рам", "лек", "бор", "вин", "гат", "зон", "лик",
+            "мек", "нор", "пан", "рак", "сам", "тор", "вал", "шер", "дим", "фир",
+            "хан", "цер", "шал", "бар", "лам", "тек", "жар", "ков", "рис", "мол"
+    };
+
+    private static final String[] LASTNAME_ENDINGS = {"ов", "ев", "ин"};
+
+    private final Random random = new Random();
+
+    @Override
+    public List<Person> load(int size) {
+        if (size <= 0) {
+            throw new IllegalArgumentException("Size must be positive");
+        }
+        List<Person> persons = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+
+            String firstName = generateFirstName();
+            String lastName = generateLastName();
+            int year = 1900 + random.nextInt(127);
+
+            persons.add(Person.builder()
+                    .firstName(capitalize(firstName))
+                    .lastName(capitalize(lastName))
+                    .year(year)
+                    .build());
+        }
+        return persons;
+    }
+
+    private String generateFirstName() {
+        return generateWorld(2);
+    }
+
+    private String generateLastName() {
+        return generateWorld(2) + LASTNAME_ENDINGS[random.nextInt(LASTNAME_ENDINGS.length)];
+    }
+
+    private String generateWorld(int syllablesCount) {
+        StringBuilder word = new StringBuilder();
+        for (int i = 0; i < syllablesCount; i++) {
+            word.append(SYLLABLES[random.nextInt(SYLLABLES.length)]);
+        }
+        return word.toString();
+    }
+
+    private String capitalize(String str) {
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+        return str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
+    }
+
+}
